68d2f5c38fec16f5c7d8c8f3ffa02e7386415975
Ivan David <davidacosta9704@gmail.com>
2020-11-16 16:18:45 -0600

    Inicio de Proyecto

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async( category ) =>{
+    
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Nap3htwanzFDiSZ01quso0yBY7NDBSWf&limit=10&q=${ encodeURI( category ) }`;
+    const resp = await fetch( url );
+    console.log(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map( img =>{
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
